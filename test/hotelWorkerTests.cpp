d55c064539f79fc8306014fe237498e0faab3c0a
Jakub Kowieski <01158762@pw.edu.pl>
2021-06-09 16:42:46 +0200

    Added tests to hotel worker

--- test/hotelWorkerTests.cpp
+++ test/hotelWorkerTests.cpp
@@ -14,3 +14,24 @@ TEST(HotelWorker, setFirstName) {
     marek.setFirstName("Marek");
     EXPECT_EQ(marek.getFirstName(), "Marek");
 }
+
+TEST(HotelWorker, setPosition) {
+    HotelWorker marek;
+
+    marek.setPosition("Maid");
+    EXPECT_EQ(marek.getPosition(), "Maid");
+}
+
+TEST(HotelWorker, setPermissionLevel) {
+    HotelWorker marek;
+
+    marek.setPermissionLevel(1);
+    EXPECT_EQ(marek.getPermissionLevel(), 1);
+}
+
+TEST(HotelWorker, setSalary) {
+    HotelWorker marek;
+
+    marek.setSalary(1000);
+    EXPECT_EQ(marek.getSalary(), 1000);
+}
